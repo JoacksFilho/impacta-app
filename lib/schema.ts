5d976674a7a4afa2e7f34f4ad18798af04f89e29
Joacks Filho <101015163+JoacksFilho@users.noreply.github.com>
2024-03-14 14:03:16 -0300

    Update schema.ts

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,25 +1,24 @@
 import {
-    pgTable,
-    serial,
-    text,
-    timestamp,
-    uniqueIndex,
-  } from "drizzle-orm/pg-core";
-  
-  export const flights = pgTable(
-    "flights",
-    {
-      flightId: serial("flightId").primaryKey(),
-      departureAirport: text("departureAirport").notNull(),
-      arrivalAirport: text("arrivalAirport").notNull(),
-      flightTime: text("flightTime").notNull(),
-      flightPrice: text("flightPrice").notNull(),
-      flightType:text("flightPrice").notNull(),
-      createdAt: timestamp("createdAt").defaultNow().notNull(),
-    },
-    (flights) => {
-      return {
-        uniqueIdx: uniqueIndex("unique_idx").on(flights.flightId),
-      };
-    }
-  );
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+
+export const flights = pgTable(
+  "flights",
+  {
+    flightId: serial("flightId").primaryKey(),
+    departureAirport: text("departureAirport").notNull(),
+    arrivalAirport: text("arrivalAirport").notNull(),
+    flightTime: text("flightTime").notNull(),
+    flightPrice: text("flightPrice").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+  },
+  (flights) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(flights.flightId),
+    };
+  }
+);
